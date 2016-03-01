62dcab99478099d7a0c352cdb1691ed830310689
William <rossgregory36@gmail.com>
2016-03-01 13:33:06 +0000

    start trainer

--- Neural_Network_Trainer/main.cpp
+++ Neural_Network_Trainer/main.cpp
@@ -8,8 +8,5 @@ int main()
     t.setup();
     t.train();
     
-    cout << endl;
-    cout << "Hello, this will be a short(i think) video where I will setup the trainer for my neural network that I created in the last series 'Create Your Own Basic Neural Network'" << endl;
-    
     return 0;
 }
